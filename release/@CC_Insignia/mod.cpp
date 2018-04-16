742fd02f40dfb74fb909101c5e2113d24e582912
Atunero <atunerin@gmail.com>
2018-04-16 20:52:09 +0200

    more work on unit patches

---
+++ release/@CC_Insignia/mod.cpp
@@ -0,0 +1,6 @@
+name = "CC Insignia";
+author = "Cavallers del Cel";
+picture = "logo_ccescut_ca.paa";
+actionName = "Portal web";
+action = "http://www.cavallersdelcel.cat";
+description = "Marcadors, banderes i pegats del grup 1RA de la Comunitat Catalana de Simulació Cavallers del Cel";
---
+++ src/mod.cpp
@@ -0,0 +1,5 @@
+name = "CC Insignia";
+picture = "logo_ccescut_ca.paa";
+actionName = "Portal web";
+action = "http://www.cavallersdelcel.cat";
+description = "Marcadors, banderes i pegats del grup 1RA de la Comunitat Catalana de Simulació Cavallers del Cel";
